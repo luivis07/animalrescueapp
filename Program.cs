66ae40ec040c961b903a1820ff7242e7d6279473
Luis Q <luivis07@gmail.com>
2022-10-08 18:22:55 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOrchardCms()
+                .AddSetupFeatures("OrchardCore.AutoSetup");
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseOrchardCore();
+
+app.Run();
