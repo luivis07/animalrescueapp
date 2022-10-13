76b64d42e0c20c81743ce010ba71b8480b7bb54c
luivis07 <luivis07@gmail.com>
2022-10-13 16:39:53 -0400

    Users/luisq/dynamic menu (#28)
    * testing stuff
    
    * major rework
    
    * removing stuff
    
    * removing stuff
    
    * checkpoint
    
    * working
    
    * checkpoint
    
    * removing stuff
    
    * final

---
+++ src/modules/animalrescue.mainmodule/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "animalrescue.mainmodule",
+    Author = "Luis Q",
+    Website = "#",
+    Version = "0.0.1",
+    Description = "Main Module Animal Rescue",
+    Category = "Content Management"
+)]
