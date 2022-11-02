4567f771c3bbff7328893b6bfcc2a3d4e0ace051
luivis07 <luivis07@gmail.com>
2022-11-02 11:24:35 -0400

    feature/userregistration/issue 43 (#65)

--- src/modules/animalrescue.mainmodule/animalrescue.mainmodule.services/Handlers/Interfaces/IAnimalRescueAccountHandler.cs
+++ src/modules/animalrescue.mainmodule/animalrescue.mainmodule.services/Handlers/Interfaces/IAnimalRescueAccountHandler.cs
@@ -7,5 +7,7 @@ namespace animalrescue.mainmodule.services.handlers.interfaces
         public Task<AnimalRescueAccountDto?> GetByUsernameAsync(string username);
         public Task<bool> UpdateAsync(AnimalRescueAccountDto animalRescueAccountDto);
         public Task<AnimalRescueAccountDto?> GetByIdAsync(int id);
+        public Task<int> CreateAsync(AnimalRescueAccountDto animalRescueAccountDto);
+        public int Create(AnimalRescueAccountDto animalRescueAccountDto);
     }
 }
