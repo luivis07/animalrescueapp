4a8ca4d38b85f041f5f0fa7cacc81ff8fc53c474
Luis Q <luivis07@gmail.com>
2022-11-05 16:12:04 -0400

    checkpoint

--- src/modules/animalrescue.mainmodule/animalrescue.mainmodule.web/Controllers/CalendarTypeController.cs
+++ src/modules/animalrescue.mainmodule/animalrescue.mainmodule.web/Controllers/CalendarTypeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace animalrescue.mainmodule.web.controllers
 {
     [Authorize]
-    public class CalendarController : BaseController
+    public class CalendarTypeController : BaseController
     {
     }
 }
---
+++ src/modules/animalrescue.mainmodule/animalrescue.mainmodule.web/Setup/Mappings/LocationMap.cs
@@ -0,0 +1,14 @@
+using animalrescue.mainmodule.services.dtos;
+using animalrescue.mainmodule.services.setup;
+using animalrescue.mainmodule.web.viewmodels.animalrescueaccount;
+using AutoMapper;
+
+namespace animalrescue.mainmodule.web.mappings
+{
+    public class LocationMap : Profile
+    {
+        public LocationMap()
+        {
+        }
+    }
+}
