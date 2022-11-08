663e9ac4a4e53e83458756fa2db5ddf348f8723f
firefly006 <jstndavis06@gmail.com>
2022-11-07 19:09:12 -0500

    checkpoint workiung on dob

--- src/modules/animalrescue.mainmodule/animalrescue.mainmodule.services/Validators/AnimalRescueAccountValidator.cs
+++ src/modules/animalrescue.mainmodule/animalrescue.mainmodule.services/Validators/AnimalRescueAccountValidator.cs
@@ -4,9 +4,35 @@ using FluentValidation;
 public class AnimalRescueAccountValidator : AbstractValidator<AnimalRescueAccountDto>
 {
     public AnimalRescueAccountValidator()
-    {   
-        RuleFor(ara => ara.Id).GreaterThan(0);
-        RuleFor(ara => ara.FirstName).NotEmpty();
-        RuleFor(ara => ara.LastName).NotEmpty();
+    {
+        RuleFor(ara => ara.Id).NotNull();
+        RuleFor(ara => ara.Username).NotNull();
+        RuleFor(ara => ara.FirstName).Length(1, 150);
+        RuleFor(ara => ara.LastName).Length(1, 150);
+        RuleFor(ara => ara.EmailAddress).EmailAddress();
+        RuleFor(ara => ara.PhoneNumber).Length(1, 150);
+        RuleFor(ara => ara.Address1).Length(1, 100);
+        RuleFor(ara => ara.Address2).Length(1, 60);
+        RuleFor(ara => ara.City).Length(1, 60);
+        RuleFor(ara => ara.State).Length(1, 50);
+        RuleFor(ara => ara.ZipCode).Length(1, 10);
+        RuleFor(ara => ara.Country).Length(1, 55);
+        RuleFor(ara => ara.DateOfBirth).Must(BeAValidAge);
+    }
+    protected bool BeAValidAge(DateTime? date)
+    {
+        if (date == null)
+        {
+            return false;
+        }
+        int currentYear = DateTime.Now.Year;
+        int dobYear = date.Value.Year;
+
+        if (dobYear <= currentYear && dobYear > (currentYear - 120))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
