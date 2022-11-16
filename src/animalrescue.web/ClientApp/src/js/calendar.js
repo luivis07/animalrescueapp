72aa9b5a2518dc67e5e70ea69dcc804b0b2e9ea8
Luis Q <luivis07@gmail.com>
2022-11-15 19:44:56 -0500

    checkpoint

---
+++ src/animalrescue.web/ClientApp/src/js/calendar.js
@@ -0,0 +1,28 @@
+import Calendar from '@toast-ui/calendar';
+import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+
+const calendar = new Calendar('#calendar', {
+    defaultView: 'week',
+    template: {
+      time(event) {
+        const { start, end, title } = event;
+  
+        return `<span style="color: white;">${formatTime(start)}~${formatTime(end)} ${title}</span>`;
+      },
+      allday(event) {
+        return `<span style="color: gray;">${event.title}</span>`;
+      },
+    },
+    calendars: [
+      {
+        id: 'cal1',
+        name: 'Personal',
+        backgroundColor: '#03bd9e',
+      },
+      {
+        id: 'cal2',
+        name: 'Work',
+        backgroundColor: '#00a9ff',
+      },
+    ],
+  });
